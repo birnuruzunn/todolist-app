c35aff065ffd4d755ed953729ade3abefe41e863
birnuruzun <birnuruzun.294976@gmail.com>
2019-04-07 02:10:34 +0000

    my todolist app, first commit

---
+++ todolist/admin.py
@@ -0,0 +1,4 @@
+from django.contrib import admin
+from .models import TodoList
+
+admin.site.register(TodoList)
---
+++ todolist/models.py
@@ -0,0 +1,15 @@
+from django.conf import settings
+from django.db import models
+from django.utils import timezone
+
+class TodoList(models.Model): 
+    title = models.CharField(max_length=250) # a varchar
+    content = models.TextField(blank=True) # a text field 
+    created_date = models.DateField(default=timezone.now().strftime("%Y-%m-%d")) 
+    due_date = models.DateField(default=timezone.now().strftime("%Y-%m-%d")) 
+    
+    def order(self):
+        return self.created_date #ordering by the created field
+
+    def __str__(self):
+        return self.title #name to be shown when called
