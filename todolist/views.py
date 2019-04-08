a5627e22b307a8af2ddca5774aec6b432f81c583
birnuruzun <birnuruzun.294976@gmail.com>
2019-04-08 01:38:45 +0000

    two pages are added

--- todolist/views.py
+++ todolist/views.py
@@ -4,6 +4,21 @@ from .models import TodoList
 def to_do_list(request):
 
 	todos = TodoList.objects.all() #quering all todos with the object manager
+	
+
+	if request.method == "POST": #checking if the request method is a POST
+		if "save" in request.POST: #checking if there is a request to add a todo
+			title = request.POST["description"] #title
+			date = str(request.POST["date"]) #date
+			
+			content = title + " -- " + date + " " 
+			Todo = TodoList(title=title, due_date=dates)
+			Todo.save() #saving the todo 
+			return redirect("/") #reloading the page
+		if "taskDelete" in request.POST: #checking if there is a request to delete a todo
+			todo = TodoList.objects.get(id) #getting todo id
+			todo.delete() #deleting todo
+
 	return render(request, 'todolist/to_do_list.html', {"todos": todos})
 
 
