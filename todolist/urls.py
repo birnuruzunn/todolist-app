63d7d217502cc9370424d39120e62e1eae4c2ff2
birnuruzun <birnuruzun.294976@gmail.com>
2019-04-08 03:39:30 +0000

    some buttons are activated

--- todolist/urls.py
+++ todolist/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.to_do_list, name='to_do_list'),
+    path('addTodo/', views.addTodo, name='addTodo'),
+    path('deleteTodo/<int:todo_id>/', views.deleteTodo, name='deleteTodo'),
 ]
--- todolist/views.py
+++ todolist/views.py
@@ -1,25 +1,41 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
 from .models import TodoList
 
 def to_do_list(request):
 
-	todos = TodoList.objects.all() #quering all todos with the object manager
-	
-
+	todos = TodoList.objects.order_by('id') #quering all todos with the object manager
+	"""
 	if request.method == "POST": #checking if the request method is a POST
 		if "save" in request.POST: #checking if there is a request to add a todo
 			title = request.POST["description"] #title
 			date = str(request.POST["date"]) #date
 			
 			content = title + " -- " + date + " " 
-			Todo = TodoList(title=title, due_date=dates)
-			Todo.save() #saving the todo 
+			new_list = TodoList(title=title, due_date=date)
+			new_list.save() #saving the todo 
 			return redirect("/") #reloading the page
-		if "taskDelete" in request.POST: #checking if there is a request to delete a todo
-			todo = TodoList.objects.get(id) #getting todo id
-			todo.delete() #deleting todo
 
+		if "taskCompleted" in request.POST:
+			completed =TodoList.objects.get('id')
+			completed.complete = True
+			completed.save()
+
+		if "taskDelete" in request.POST: #checking if there is a request to delete a todo
+			delete_list = TodoList.objects.get('id') #getting todo id
+			delete_list.delete() #deleting todo
+	"""
 	return render(request, 'todolist/to_do_list.html', {"todos": todos})
 
+def addTodo(request):
+	title = request.POST["title"] #title
+	date = str(request.POST["date"]) #date
+	content = title + " -- " + date + " " 
 
-
+	new_list = TodoList(title=title,  content=content, due_date=date)
+	new_list.save() #saving the todo
+	return HttpResponseRedirect('/')
+def deleteTodo(request, todo_id):
+	delete_list = TodoList.objects.get(id=todo_id) #getting todo id
+	delete_list.delete() #deleting todo
+	return HttpResponseRedirect('/')
